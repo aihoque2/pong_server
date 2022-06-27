2bd5dfc91f34d8ecf644e751a5a3c12bb641513c
aihoque2 <ahoque245@gmail.com>
2022-06-27 04:38:13 -0400

    test done. time to compile

--- SDLTest.cpp
+++ SDLTest.cpp
@@ -20,7 +20,47 @@ int main(){
     SDL_Surface* screen = NULL;
 
     //load the bitmap
+    SDL_Surface* temp = SDL_LoadBMP("sdl_logo.bmp");
 
+    //convert bitmap to converted
+    SDL_Surface* converted = SDL_DisplayFormat(temp);
 
+    SDL_Event event;
+    int gameover = 0;
+
+    while (!gameover){
+        //search for event
+        if (SDL_PollEvent(&event)){//TODO: WTF is a PollEevent?
+            switch(event.type){
+                //close button clicked
+                case SDL_QUIT: //TODO: what is this case?
+                    gameover = 1;
+                    break;
+
+                //keyboard case
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.sym){
+                        case SDLK_ESCAPE:
+                            case SDLK_q:
+                                gameover = 1;
+                                break;
+                    }
+                    break;
+            }
+        }
+
+        //draw background
+        SDL_BlitSurface(converted, NULL, screen, NULL);
+
+        //update screen
+        SDL_UpdateRect(screen, 0, 0, 0, 0);
+
+    }
+
+    //free background surface
+    SDL_FreeSurface(converted)
+
+    //cleanup sdl
+    SDL_Quit();
     return 0;
 }
