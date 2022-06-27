17daf37ce8f236b4b8c76997ca307d165c88cb41
aihoque2 <ahoque245@gmail.com>
2022-06-27 04:24:42 -0400

    light work on sdltest

--- SDLTest.cpp
+++ SDLTest.cpp
@@ -8,5 +8,19 @@ and use the "gamedevgeek" tutorial
 */
 
 int main(){
+    /*initalize sdl*/
+    int init_status = SDL_Init(SDL_INIT_VIDEO);
+
+    if (init_status < 0){ return 0;} //TODO: set error catching
+
+    //title bar
+    SDL_WM_SetCaption("SDL Test", "SDL TEST");
+
+    //create window
+    SDL_Surface* screen = NULL;
+
+    //load the bitmap
+
+
     return 0;
 }
